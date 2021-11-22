de54c8e42a6abf50d02ffb8c38b8e8b317a15690
Scott Morrison <scott.morrison@gmail.com>
2021-11-22 14:57:03 +1100

    feat: also download leans (#1)

--- lakefile.lean
+++ lakefile.lean
@@ -3,34 +3,49 @@ open Lake DSL System
 
 def tag : String := "nightly-2021-11-17"
 def releaseRepo : String := "leanprover-community/mathport"
-def tarName : String := "mathlib3-binport.tar.gz"
+def oleanTarName : String := "mathlib3-binport.tar.gz"
+def leanTarName : String := "mathlib3-synport.tar.gz"
+
+def download (url : String) (to : FilePath) : BuildM PUnit := Lake.proc
+{ -- We use `curl -O` to ensure we clobber any existing file.
+  cmd := "curl",
+  args := #["-L", "-O", url]
+  cwd := to }
+
+def untar (file : FilePath) : BuildM PUnit := Lake.proc
+{ cmd := "tar",
+  args := #["-xzvf", file.fileName.getD "."] -- really should throw an error if `file.fileName = none`
+  cwd := file.parent }
+
+def getReleaseArtifact (repo tag artifact : String) (to : FilePath) : BuildM PUnit :=
+download s!"https://github.com/{repo}/releases/download/{tag}/{artifact}" to
+
+def untarReleaseArtifact (repo tag artifact : String) (to : FilePath) : BuildM PUnit := do
+  getReleaseArtifact repo tag artifact to
+  untar (to / artifact)
 
 def fetchOleans (dir : FilePath) : OpaqueTarget := { info := (), task := fetch } where
   fetch := async do
     IO.FS.createDirAll libDir
     let oldTrace := Hash.ofString (← Git.headRevision dir)
-    buildFileUnlessUpToDate (libDir / tarName) oldTrace do
-      downloadOleans
-      untarOleans
-
-  downloadOleans : BuildM PUnit := Lake.proc {
-      cmd := "wget",
-      args := #[s!"https://github.com/{releaseRepo}/releases/download/{tag}/{tarName}"]
-      cwd := libDir.toString
-    }
-
-  untarOleans : BuildM PUnit := Lake.proc {
-      cmd := "tar",
-      args := #["-xzvf", tarName]
-      cwd := libDir.toString
-    }
+    buildFileUnlessUpToDate (libDir / oleanTarName) oldTrace do
+      untarReleaseArtifact releaseRepo tag oleanTarName libDir
 
   libDir : FilePath := dir / "build" / "lib"
+
+def fetchLeans (dir : FilePath) : OpaqueTarget := { info := (), task := fetch } where
+  fetch := async do
+    IO.FS.createDirAll srcDir
+    let oldTrace := Hash.ofString (← Git.headRevision dir)
+    buildFileUnlessUpToDate (srcDir / leanTarName) oldTrace do
+      untarReleaseArtifact releaseRepo tag leanTarName srcDir
+
+  srcDir : FilePath := dir
 
 package mathlib3port (dir) {
   libRoots := #[]
   libGlobs := #[`Mathbin]
-  extraDepTarget := fetchOleans dir
+  extraDepTarget := Target.collectOpaqueList [fetchLeans dir, fetchOleans dir]
   defaultFacet := PackageFacet.oleans
   dependencies := #[{
     name := "lean3port",
