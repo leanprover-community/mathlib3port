2c527660a1ac0f7301ee5fff8a7ab4332f2985e7
Gabriel Ebner <gebner@gebner.org>
2022-04-29 20:20:06 +0200

    Add update script.

---
+++ update.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+echo Updates to the latest revision in the lean3port repo
+echo and creates a bump commit.
+
+set -ex
+
+pushd lean_packages/lean3port
+git fetch origin
+git checkout origin/master
+cp lean-toolchain ../..
+lean3port_rev=$(git rev-parse HEAD)
+tag=$(sed '/^def tag /!d;s/.*"\(.*\)"$/\1/' lakefile.lean)
+popd
+
+sed -i '
+  /^def tag / s/"\(.*\)"$/"'$tag'"/;
+  /src.*:=.*lean3port/ s/"\([^"]*\)"$/"'$lean3port_rev'"/
+' lakefile.lean
+
+rm -rf Mathbin
+curl -qsSL https://github.com/leanprover-community/mathport/releases/download/$tag/mathlib3-synport.tar.gz \
+  | tar xz
+
+git add Mathbin
+git commit -am "bump to $tag"
