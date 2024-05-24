fe6f4ca49bd62782d8b12402b9c036bb4021635d
Mario Carneiro <di.gama@gmail.com>
2024-05-24 10:36:38 +0200

    chore: update lean

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,7 @@ open Lake DSL System
 -- Usually the `tag` will be of the form `nightly-2021-11-22`.
 -- If you would like to use an artifact from a PR build,
 -- it will be of the form `pr-branchname-sha`.
-def tag : String := "nightly-2024-05-22-08"
+def tag : String := "nightly-2024-05-23-08"
 def releaseRepo : String := "leanprover-community/mathport"
 def oleanTarName : String := "mathlib3-binport.tar.gz"
 
@@ -23,16 +23,16 @@ def untarReleaseArtifact (repo tag artifact : String) (to : FilePath) : JobM PUn
 package mathlib3port where
   extraDepTargets := #[`fetchOleans]
 
-target fetchOleans (_pkg) : Unit := do
+target fetchOleans (_pkg) : Unit := Job.async do
   let libDir : FilePath := __dir__ / ".lake" / "build" / "lib"
   IO.FS.createDirAll libDir
   let oldTrace := Hash.ofString tag
   let _ ← buildFileUnlessUpToDate (libDir / oleanTarName) oldTrace do
     logInfo "Fetching oleans for Mathbin"
     untarReleaseArtifact releaseRepo tag oleanTarName libDir
-  return .nil
+  return ((), .nil)
 
-require lean3port from git "https://github.com/leanprover-community/lean3port.git"@"83a42a6eab93450e18f84040be47cdb1b2e40b18"
+require lean3port from git "https://github.com/leanprover-community/lean3port.git"@"0a9ff50c59c1f718dff35efaeba330035315527e"
 
 @[default_target]
 lean_lib Mathbin where
